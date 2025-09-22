2d387465ac0f1ca04d12fb35312cd3e95ce73d1d
ASHAN NUWANTHA <ashannuwantha050@gmail.com>
2025-09-22 15:53:25 +0530

    add apk to build file

--- app.config.js
+++ app.config.js
@@ -1,5 +1,24 @@
 export default {
-  name: "FloraLink",
-  slug: "flora-link",
-  version: "1.0.0",
-}
+  expo: {
+    name: "FloraLink",
+    slug: "flora-link",
+    version: "1.0.0",
+    orientation: "portrait",
+    userInterfaceStyle: "light",
+    updates: {
+      fallbackToCacheTimeout: 0,
+    },
+    assetBundlePatterns: ["**/*"],
+    ios: {
+      supportsTablet: true,
+    },
+    android: {
+      package: "com.ashannuwantha.floralink",
+    },
+    extra: {
+      eas: {
+        projectId: "5547ed37-cf13-4a94-856f-75f03acd46ba",
+      },
+    },
+  },
+};
